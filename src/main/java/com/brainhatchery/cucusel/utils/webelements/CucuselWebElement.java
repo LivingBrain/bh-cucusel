e8d681418a6f29aa1b1543bca0dbc5dc233f4b30
LivingBrain <realcookie@yahoo.com>
2023-12-20 17:05:13 +0100

    Added methods to click and wait for request

--- src/main/java/com/brainhatchery/cucusel/utils/webelements/CucuselWebElement.java
+++ src/main/java/com/brainhatchery/cucusel/utils/webelements/CucuselWebElement.java
@@ -8,18 +8,29 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.awaitility.Awaitility;
+import org.awaitility.core.ConditionTimeoutException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.v109.network.Network;
+import org.openqa.selenium.devtools.v109.network.model.RequestWillBeSentExtraInfo;
+import org.openqa.selenium.devtools.v109.network.model.ResponseReceived;
+import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Extend with this calss for any custom web element classes
+ *
  * @param <T> - custom class name
  */
 @Getter
@@ -49,6 +60,7 @@ public abstract class CucuselWebElement<T extends CucuselWebElement<T>> {
 
     /**
      * Use this method to provide string parameters
+     *
      * @param args - Array of string parameters values
      * @return - parent class so method chaining may be used.
      */
@@ -59,7 +71,8 @@ public abstract class CucuselWebElement<T extends CucuselWebElement<T>> {
 
     /**
      * Methond will return Selenium WebElement
-     * @return  WebElement
+     *
+     * @return WebElement
      */
     public WebElement getWebElement() {
         return driver.findElement(by);
@@ -67,6 +80,7 @@ public abstract class CucuselWebElement<T extends CucuselWebElement<T>> {
 
     /**
      * Method will return collection of Selenium WebElements
+     *
      * @return List<WebElement>
      */
     public List<WebElement> getWebElements() {
@@ -118,8 +132,65 @@ public abstract class CucuselWebElement<T extends CucuselWebElement<T>> {
     }
 
     public T setElementTimeout(long explicitTimeout) {
+        this.explicitTimeout = explicitTimeout;
         wait.withTimeout(Duration.ofSeconds(explicitTimeout));
         return (T) this;
+    }
+
+    public RequestWillBeSentExtraInfo clickAndInterceptRequest(String requestMethod) {
+        final List<RequestWillBeSentExtraInfo> savedRequest = new ArrayList<>();
+        WebDriver webDriver = new Augmenter().augment(driver);
+
+        DevTools devTools = ((HasDevTools) webDriver).getDevTools();
+        devTools.createSession();
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+        devTools.addListener(Network.requestWillBeSentExtraInfo(), request -> {
+            if (request.getHeaders().toJson().get(":method").equals(requestMethod)) {
+                savedRequest.add(request);
+            }
+        });
+        driver.findElement(by).click();
+        try {
+            Awaitility.await().pollThread(Thread::new).atMost(explicitTimeout, TimeUnit.SECONDS).until(() -> savedRequest.size() >= 1);
+        } catch (ConditionTimeoutException e) {
+            throw new ConditionTimeoutException(String.format("No matching request found. Expected request method: %s", requestMethod));
+        }
+        devTools.send(Network.disable());
+        devTools.clearListeners();
+        devTools.close();
+        return savedRequest.get(0);
+    }
+
+
+    public String clickAndWaitForResponse(String endpointKey, int statusCode) {
+        final List<ResponseReceived> savedResponses = new ArrayList<>();
+        final String[] responseBody = new String[1];
+        final Integer[] responseStatusCode = new Integer[1];
+        WebDriver webDriver = new Augmenter().augment(driver);
+
+        DevTools devTools = ((HasDevTools) webDriver).getDevTools();
+        devTools.createSession();
+        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+        devTools.addListener(Network.responseReceived(), received -> {
+            if (received.getResponse().getUrl().contains(endpointKey)) {
+                responseBody[0] = devTools.send(Network.getResponseBody(received.getRequestId())).getBody();
+                responseStatusCode[0] = received.getResponse().getStatus();
+                if (responseStatusCode[0] == statusCode) {
+                    savedResponses.add(received);
+                }
+            }
+        });
+        driver.findElement(by).click();
+        try {
+            Awaitility.await().pollThread(Thread::new).atMost(explicitTimeout, TimeUnit.SECONDS).until(() -> savedResponses.size() >= 1);
+        } catch (ConditionTimeoutException e) {
+            throw new ConditionTimeoutException(String.format("Expected status code: %s. Actual status code: %s. With body: %s",
+                    statusCode, responseStatusCode[0], responseBody[0]));
+        }
+        devTools.send(Network.disable());
+        devTools.clearListeners();
+        devTools.close();
+        return responseBody[0];
     }
 
     private WebElement waitForClickableWebElement() {
